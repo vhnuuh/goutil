757f022dbcb99d639b5ccf22f1990f462df2e77a
江帆 <zn_jiangf@corp.21cn.com>
2022-05-23 15:38:16 +0800

    twto: chapter4

---
+++ twtg/ch4/time.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var week time.Duration
+
+func main() {
+	t := time.Now()
+	fmt.Printf("%02d.%02d.%4d\n", t.Day(), t.Month(), t.Year())
+	t = time.Now().UTC()
+	fmt.Println(t)
+	fmt.Println(time.Now())
+
+	week = 60 * 60 * 24 * 7 * 1e9 // must be in nanosec
+	week_from_now := t.Add(time.Duration(week))
+	fmt.Println(week_from_now)
+	// formatting times:
+	fmt.Println(t.Format(time.RFC822))
+	fmt.Println(t.Format(time.ANSIC))
+	fmt.Println(t.Format("02 Jan 2006 15:04"))
+	s := t.Format("2006-01-02 15:04:05")
+	fmt.Println(t, "=>", s)
+}
